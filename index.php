ad1e313bc69395715f1673248d9ced306be4b3b7
legion <Vlad.Ionash@gmail.com>
2019-08-16 23:26:30 +0300

    Первый комит

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php 
+
+include_once 'function.php';
+include_once 'bd.php';
+
+
+$a = +5;
+
+$a= +10;
+
+$a = +5;
+$a = +5;
+
+echo $a.'<br>';
+
+
+$new_gas= 10+19+1;
+
+$gas = 10+11+$a;
+
+
+
+
+?>
